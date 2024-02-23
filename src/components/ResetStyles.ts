c665c39179b522296fff9a9418fb885c10b09625
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-02-23 15:03:20 +0100

    Format reset styles

--- src/components/ResetStyles.ts
+++ src/components/ResetStyles.ts
@@ -1,13 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const ResetStyles = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
   }
 `;
 
